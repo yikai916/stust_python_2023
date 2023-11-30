c731b39c31886595d13f508a321b4d184142b729
kevinnn <114348654+yikai916@users.noreply.github.com>
2023-11-30 12:01:06 +0800

    exercise

--- exercise_1130/exercise1.py
+++ exercise_1130/exercise1.py
@@ -11,23 +11,43 @@ class Student:
 
     def getschool(self):
         return self.school
-    def setschool(self):
-        return self.school
+    def setschool(self, new_school):
+        self.school = new_school
 
     def getmajor(self):
         return self.major
-    def setmajor(self):
-        return self.major
+    def setmajor(self, new_major):
+        self.major = new_major
 
     def getteacher(self):
         return self.teacher
-    def setteacher(self):
-        return self.teacher
+    def setteacher(self, new_teacher):
+        self.teacher = new_teacher
 
     def getname(self):
         return self.name
-    def setname(self):
-        return self.name
+    def setname(self, new_name):
+        self.name = new_name
+
+    def getstudentID(self):
+        return self.studentID
+    def setstudentID(self, new_studentID):
+        self.studentID = new_studentID
+
+    def getaddress(self):
+        return self.address
+    def setaddress(self, new_address):
+        self.address = new_address
+
+    def getcredit(self):
+        return self.credit
+    def setcredit(self, new_credit):
+        self.credit = new_credit
+
+    def getscore(self):
+        return self.score
+    def setscore(self, new_score):
+        self.score = new_score
 
     def printAll(self):
         print(self.getschool())
@@ -38,3 +58,5 @@ class Student:
 kevinnn = Student('STUST', 'CSIE', 'Micheal', 'Kevinnn', '4B0G0112', 'Tainan', '100', '10')
 
 Student.printAll(kevinnn)
+
+
