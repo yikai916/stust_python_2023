953b029407fb71dced6b7b32ba4c67f7cf9dc8f6
kevinnn <114348654+yikai916@users.noreply.github.com>
2023-12-14 11:56:05 +0800

    wrong

---
+++ exercise_1214/exercise1.py
@@ -0,0 +1,34 @@
+class Student:
+    def __init__(self, name, identify, major):
+        self.name = name
+        self.identify = identify
+        self.major = major
+    
+    def prinntAll(self):
+        print(self.name)
+        print(self.identify)
+        print(self.major)
+
+class Course(Student):
+    def __init__(self, name, identify, major, course):
+        super().__init__(name, identify, major)
+        self.course = course
+    
+    def addCourse(self, course, semester):
+        self.course = course
+        self.semester = semester
+        
+    def checkCourse(self):
+        self.addCourse(self.course, self.semester)
+        print(self.Course)
+        print(self.semester)
+
+obj1 = Student('kevinnn', 'ab1234', 'CSIE')
+obj1.prinntAll()
+
+obj2 = Course('kevinnn', 'ab5678', 'CSIE', 'math')
+obj2.prinntAll()
+print(obj2.course)
+
+obj2.addCourse('physics', 'Spring 2023')
+obj2.checkCourse()
